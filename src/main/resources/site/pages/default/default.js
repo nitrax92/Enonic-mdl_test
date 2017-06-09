04c23d78d4810e8aa1032346cc41ab80d5d6e3a0
Nitrax <Martin.Engen@outlook.com>
2017-06-09 16:31:20 +0200

    Color Scheme, Background Image
    Color Scheme is based on creators prefference, and can be chosen when editing the page. It works by simply loading a different style file for the Material Design Lite framework.
    
    Background Image can be chosen by creator.

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -17,13 +17,23 @@ exports.get = function (req) {
     };
     */
 
+    //var test_url = req.assetUrl();
+    //log.info(test_url);
     var mainRegion = content.page.regions['mdl-content'];
+    //var background_image = libs.portal.getComponent().config;
+    // Find the current component from request
+    //var component = libs.portal
 
+    var title = site.page.config['title'] || 'Default Title';
+    var background_image = site.page.config['background-image'];
+    var color_scheme = site.page.config['color_scheme'] || 'indigo-pink';
 
     // Prepare the model that will be passed to the view
     var model = {
         mainRegion: mainRegion,
-        pageTitle: "My Title"
+        title: "My Title",
+        bg_img: background_image,
+        scheme: color_scheme
     };
 
     var view = resolve('default.html');
