84abd6c735228d771dddd95a98a807b8353d9831
Nitrax <Martin.Engen@outlook.com>
2017-06-12 20:54:16 +0200

    Cont. Slick + MDL parts
    accidentally worked on some parts from the MDL library, but cba to create a new branch.

--- src/main/resources/site/pages/default/default.js
+++ src/main/resources/site/pages/default/default.js
@@ -2,8 +2,8 @@ var libs = {
     portal: require('/lib/xp/portal'), //??
     thymeleaf: require('/lib/xp/thymeleaf') // Templateing engine
 };
-
 exports.get = function (req) {
+    log.info("DEFAULT PAGE LOADING...");
     // Page Content
     var content = libs.portal.getContent();
     var site = libs.portal.getSite();
@@ -16,6 +16,7 @@ exports.get = function (req) {
         pageTitle: pageTitle
     };
     */
+    var moment = require('/assets/momentjs/2.14.1/min/moment-with-locales.min.js');
 
     //var test_url = req.assetUrl();
     //log.info(test_url);
@@ -23,6 +24,8 @@ exports.get = function (req) {
     //var background_image = libs.portal.getComponent().config;
     // Find the current component from request
     //var component = libs.portal
+    //log.error('My JSON %s', mainRegion );
+    //var angular = require('/assets/angular/1.6.4/angular.js');
 
     var title = site.page.config['title'] || 'Default Title';
     var background_image = site.page.config['background-image'];
@@ -37,7 +40,7 @@ exports.get = function (req) {
     };
 
     var view = resolve('default.html');
-
+    log.info("DEFAULT PAGE DONE");
     // Return the merged view and model in the response obj.
     return {
         body: libs.thymeleaf.render(view, model)
